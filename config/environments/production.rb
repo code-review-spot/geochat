3136d63849eee93d3834ab9a98bb2c28036dab42
Nate Goldman <nnmgoldman@gmail.com>
2012-05-03 12:59:20 -0700

    pusher production app id

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Geochat::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  require 'pusher'
+
+  Pusher.app_id = '19783'
 
   # Code is not reloaded between requests
   config.cache_classes = true
